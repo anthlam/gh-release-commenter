77e1d9f20da869b2fcef22dc2e5f45bfc9d9f9e4
Anthony Lamorte <lamorte@gmail.com>
2016-12-01 15:53:16 -0500

    Minor changes to MergedPullRequestFinder structure
    Change messages displayed, and put instance variable assignment with method doing work.

--- lib/merged_pull_request_finder.rb
+++ lib/merged_pull_request_finder.rb
@@ -2,32 +2,32 @@ class MergedPullRequestFinder
   MERGED_PR_MESSAGE="Merge pull request #"
   MERGED_PR_REGEX=/Merge pull request #(\d*)/i
 
-  def initialize(repo, last_deploy)
+  def initialize(repo, target)
     @repo = repo
-    @target = last_deploy
+    @target = target
   end
 
   def merged_pr_numbers
-    @merge_commits = merge_commits_since_last_deploy
-    @commit_messages = commit_messages
+    merge_commits_since_target
+    commit_messages
     pr_numbers
   end
 
   private
 
-  def merge_commits_since_last_deploy
-    puts "Diffing git log, searching for commits with '#{MERGED_PR_MESSAGE}'"
-    @repo.log.between(@target).grep(MERGED_PR_MESSAGE)
+  def merge_commits_since_target
+    puts "Diffing git log, searching for commit messages containing '#{MERGED_PR_MESSAGE}'"
+    @merge_commits = @repo.log.between(@target).grep(MERGED_PR_MESSAGE)
   end
 
   def commit_messages
-    @merge_commits.map do |c|
+    @commit_messages = @merge_commits.map do |c|
       c.message
     end
   end
 
   def pr_numbers
-    puts "Getting array of PR numbers from messages in list of commits"
+    puts "Getting PR numbers from commit messages"
     @commit_messages.map do |m|
       m.scan(MERGED_PR_REGEX)
     end.flatten.uniq
